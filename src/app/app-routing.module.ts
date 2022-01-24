695cd0b3e9cf6c27f32abc56311b6d3888345da2
kazuyuki-mera <49195547+kazuyuki-mera@users.noreply.github.com>
2022-01-24 23:42:02 +0900

    商品詳細ページ作成

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { ProductListComponent } from './product-list/product-list.component';
 
-const routes: Routes = [{ path: '', component: ProductListComponent }];
+const routes: Routes = [
+  { path: '', component: ProductListComponent },
+  { path: 'detail', component: ProductDetailComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
