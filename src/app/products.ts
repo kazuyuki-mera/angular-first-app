af8e7d313a98be7f87a645a854e725ad10b6b63a
kazuyuki-mera <49195547+kazuyuki-mera@users.noreply.github.com>
2022-01-27 00:46:44 +0900

    商品詳細ページの修正

--- src/app/products.ts
+++ src/app/products.ts
@@ -3,20 +3,56 @@ export const products = [
     name: 'Phone XL',
     price: 799,
     description: 'A large phone with one of the best screens',
+    heading1: 'sample text 1',
+    heading2: 'sample text 2',
+    heading3: 'sample text 3',
+    headingText1:
+      'sample text 1 sample text 1 sample text 1 sample text 1 sample text 1 sample text 1 ',
+    headingText2:
+      'sample text 2 sample text 2 sample text 2 sample text 2 sample text 2 sample text 2 ',
+    headingText3:
+      'sample text 3 sample text 3 sample text 3 sample text 3 sample text 3 sample text 3 ',
   },
   {
     name: 'Phone Mini',
     price: 699,
     description: 'A great phone with one of the best cameras',
+    heading1: 'sample text 1',
+    heading2: 'sample text 2',
+    heading3: 'sample text 3',
+    headingText1:
+      'sample text 1 sample text 1 sample text 1 sample text 1 sample text 1 sample text 1 ',
+    headingText2:
+      'sample text 2 sample text 2 sample text 2 sample text 2 sample text 2 sample text 2 ',
+    headingText3:
+      'sample text 3 sample text 3 sample text 3 sample text 3 sample text 3 sample text 3 ',
   },
   {
     name: 'Phone Standard',
     price: 299,
     description: '',
+    heading1: 'sample text 1',
+    heading2: 'sample text 2',
+    heading3: 'sample text 3',
+    headingText1:
+      'sample text 1 sample text 1 sample text 1 sample text 1 sample text 1 sample text 1 ',
+    headingText2:
+      'sample text 2 sample text 2 sample text 2 sample text 2 sample text 2 sample text 2 ',
+    headingText3:
+      'sample text 3 sample text 3 sample text 3 sample text 3 sample text 3 sample text 3 ',
   },
   {
     name: 'iPhone 12',
     price: 299,
     description: '',
+    heading1: 'sample text 1',
+    heading2: 'sample text 2',
+    heading3: 'sample text 3',
+    headingText1:
+      'sample text 1 sample text 1 sample text 1 sample text 1 sample text 1 sample text 1 ',
+    headingText2:
+      'sample text 2 sample text 2 sample text 2 sample text 2 sample text 2 sample text 2 ',
+    headingText3:
+      'sample text 3 sample text 3 sample text 3 sample text 3 sample text 3 sample text 3 ',
   },
 ];
