6c00ffbaa8b6fb25bfdd29f300c47d24d9c7de77
kazuyuki-mera <49195547+kazuyuki-mera@users.noreply.github.com>
2022-01-31 00:20:48 +0900

    MongoDbにサンプルデータを保存

--- server/index.js
+++ server/index.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const config = require("./config/dev");
+const FakeDb = require("./fake-db");
 const app = express();
 
-mongoose.connect(config.DB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose
+  .connect(config.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    const fakeDB = new FakeDb();
+    fakeDB.seeDb();
+  });
 
 app.get("/products", function (req, res) {
   res.json({ success: true });
