4562686808d906f61b723ed659e55d13038729b6
kazuyuki-mera <49195547+kazuyuki-mera@users.noreply.github.com>
2022-02-01 23:21:00 +0900

    コミット漏れ対応

--- src/app/product/product-detail/product-detail.component.ts
+++ src/app/product/product-detail/product-detail.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
-import { products } from '../../products';
 import { ProductService } from '../shared/product.service';
 
 @Component({
