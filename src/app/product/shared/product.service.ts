7e1b63915d5188adf5cbfdcb480ae8f7875e82c1
kazuyuki-mera <49195547+kazuyuki-mera@users.noreply.github.com>
2022-02-02 00:06:28 +0900

    フロントエンドとバックエンドの繋ぎ込み

--- src/app/product/shared/product.service.ts
+++ src/app/product/shared/product.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
 import { products } from 'src/app/products';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class ProductService {
-  getProducts() {
-    return products;
+  constructor(private http: HttpClient) {}
+  getProducts(): Observable<any> {
+    return this.http.get('/api/v1/products');
   }
 
   getProductById(productId: number) {
