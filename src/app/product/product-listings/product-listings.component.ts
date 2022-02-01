8f0c7d2580661ff4458a77299b0f9fa33ee97199
kazuyuki-mera <49195547+kazuyuki-mera@users.noreply.github.com>
2022-02-01 23:38:30 +0900

    Observableを使ったサンプルプログラム作成

--- src/app/product/product-listings/product-listings.component.ts
+++ src/app/product/product-listings/product-listings.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../shared/product.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-product-listings',
@@ -12,5 +13,29 @@ export class ProductListComponent implements OnInit {
 
   ngOnInit(): void {
     this.products = this.productService.getProducts();
+
+    const observable = new Observable((subscriber) => {
+      subscriber.next(1);
+      subscriber.next(2);
+      subscriber.next(3);
+      setTimeout(() => {
+        subscriber.next(4);
+        subscriber.complete();
+      }, 1000);
+    });
+
+    console.log('just before subscribe');
+    observable.subscribe({
+      next(data) {
+        console.log('got value' + data);
+      },
+      error(err) {
+        console.error('something wrong occurred' + err);
+      },
+      complete() {
+        console.log('done');
+      },
+    });
+    console.log('just after subscribe');
   }
 }
