70bc79e2fc30efbe7f0afc2f20cd438ec5f9d15c
kazuyuki-mera <49195547+kazuyuki-mera@users.noreply.github.com>
2022-02-06 16:03:17 +0900

    Userモデル作成

---
+++ server/model/user.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  userName: {
+    type: String,
+    required: true,
+    max: [60, "ユーザー名は最大60文字までです"],
+  },
+  email: {
+    type: String,
+    required: true,
+    lowercase: true,
+    unique: true,
+    max: [60, "Eメールは最大60文字までです"],
+  },
+  password: {
+    type: String,
+    required: true,
+    min: [6, "60文字以上で入力してください"],
+    max: [30, "最大30文字までです"],
+  },
+});
+
+module.exports = mongoose.model("User", UserSchema);
