54d3b96aae7c1b3ce0ac93c059df8ec2092fb5fa
kazuyuki-mera <49195547+kazuyuki-mera@users.noreply.github.com>
2022-02-06 18:34:20 +0900

    フローチャートに従って実装

--- server/routes/products.js
+++ server/routes/products.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const product = require("../model/product");
+const Product = require("../model/product");
 
 router.get("", function (req, res) {
-  product.find({}, function (err, foundProducts) {
+  Product.find({}, function (err, foundProducts) {
     return res.json(foundProducts);
   });
 });
 
 router.get("/:productId", function (req, res) {
   const productId = req.params.productId;
-  product.findById(productId, function (err, foundProduct) {
+  Product.findById(productId, function (err, foundProduct) {
     if (err) {
       return res.status(422).send({
         errors: [{ title: "Product error" }, { detail: "Product not found" }],
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,26 +1,40 @@
 const express = require("express");
 const router = express.Router();
-const user = require("../model/user");
+const User = require("../model/user");
 
-router.post("/login", function (req, res) {
-  // product.find({}, function (err, foundProducts) {
-  //   return res.json(foundProducts);
-  // });
-});
+router.post("/login", function (req, res) {});
 
 router.post("/register", function (req, res) {
-  const username = req.body.username;
+  const userName = req.body.userName;
   const email = req.body.email;
   const password = req.body.password;
-  // const productId = req.params.productId;
-  // product.findById(productId, function (err, foundProduct) {
-  //   if (err) {
-  //     return res.status(422).send({
-  //       errors: [{ title: "Product error" }, { detail: "Product not found" }],
-  //     });
-  //   }
-  return res.json({ username, email, password });
-  // });
+  const confirmPassword = req.body.confirmPassword;
+
+  if (!userName) {
+    // Invalid Error
+  }
+  if (!email) {
+    // Invalid Error
+  }
+  if (!password) {
+    // Invalid Error
+  }
+  if (password !== confirmPassword) {
+    // Invalid Error
+  }
+
+  User.findOne({ email }, function (err, foundUser) {
+    if (foundUser) {
+      // Invalid Error
+    }
+    const user = new User({ userName, email, password });
+    user.save(function (err) {
+      if (err) {
+        // Error Message
+      }
+      return res.json({ registered: true });
+    });
+  });
 });
 
 module.exports = router;
