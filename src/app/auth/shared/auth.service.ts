c20a6dd96303334fecd0fbf7f6e1020173e7c0df
kazuyuki-mera <49195547+kazuyuki-mera@users.noreply.github.com>
2022-02-12 17:29:09 +0900

    localStorageにmetaデータを設定

--- src/app/auth/shared/auth.service.ts
+++ src/app/auth/shared/auth.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { JwtHelperService } from '@auth0/angular-jwt';
+
+const jwt = new JwtHelperService();
 @Injectable()
 export class AuthService {
   constructor(private http: HttpClient) {}
@@ -13,7 +16,9 @@ export class AuthService {
   login(userData: any): Observable<any> {
     return this.http.post('/api/v1/users/login', userData).pipe(
       map((token: string) => {
+        const decodedToken = jwt.decodeToken(token);
         localStorage.setItem('app-auth', token);
+        localStorage.setItem('app-meta', JSON.stringify(decodedToken));
       })
     );
   }
